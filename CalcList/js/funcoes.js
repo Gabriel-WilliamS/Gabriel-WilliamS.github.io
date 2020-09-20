7a3177146dd0634b47eec7d7a27e7abff9af7d14
amvinha <gabrielwilliamcps@gmail.com>
2020-09-20 01:21:57 -0300

    atualizado

--- CalcList/js/funcoes.js
+++ CalcList/js/funcoes.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 function lightMode() {
 
   var darkmode =   document.querySelector(".servicos");
@@ -11,22 +9,4 @@ function lightMode() {
   document.querySelector("#limpar-imc").classList.toggle("btn-dark");
   document.querySelector("#calcular-imc").classList.toggle("btn-light");
   document.querySelector("#calcular-imc").classList.toggle("btn-dark");
- 
-  
-=======
-
-function lightMode() {
-
-  var darkmode =   document.querySelector(".servicos");
-  darkmode.classList.toggle("dark-mode");
-
-  document.querySelector("body").classList.toggle("dark-mode-body");
-  document.querySelector(".medirImc").classList.toggle("dark-mode-imc");
-  document.querySelector("#limpar-imc").classList.toggle("btn-light");
-  document.querySelector("#limpar-imc").classList.toggle("btn-dark");
-  document.querySelector("#calcular-imc").classList.toggle("btn-light");
-  document.querySelector("#calcular-imc").classList.toggle("btn-dark");
- 
-  
->>>>>>> 0f17eac4f4d072ae79e471dbfc52e83d8569034d
 }
--- Calculadora/meu_script.js
+++ Calculadora/meu_script.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-  
 function calcular(tipo, valor) {
  
     if (tipo === 'acao') {
@@ -22,29 +20,4 @@ function calcular(tipo, valor) {
     else if (tipo === 'valor') {
         document.getElementById('resultado').value += valor
     }
-=======
-  
-function calcular(tipo, valor) {
- 
-    if (tipo === 'acao') {
-
-        if (valor === 'c') {
-            document.getElementById('resultado').value = ''
-        }
-
-        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
-            document.getElementById('resultado').value += valor
-        }
-
-        if (valor === '=') {
-            var valor_campo = eval(document.getElementById('resultado').value)
-
-            document.getElementById('resultado').value = valor_campo
-        }
-    } 
-    
-    else if (tipo === 'valor') {
-        document.getElementById('resultado').value += valor
-    }
->>>>>>> 0f17eac4f4d072ae79e471dbfc52e83d8569034d
 }
