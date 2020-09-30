fb78f9cc17bbe1b204f692c76b0003442be258ac
amvinha <gabrielwilliamcps@gmail.com>
2020-09-30 20:02:10 -0300

    Atualizdo..

--- CalcList/js/funcoes.js
+++ CalcList/js/funcoes.js
@@ -9,4 +9,11 @@ function lightMode() {
   document.querySelector("#limpar-imc").classList.toggle("btn-dark");
   document.querySelector("#calcular-imc").classList.toggle("btn-light");
   document.querySelector("#calcular-imc").classList.toggle("btn-dark");
+  document.querySelector(".entradas0").classList.toggle("bg-dark");
+  document.querySelector(".entradas1").classList.toggle("bg-dark");
+  document.querySelector(".entradas2").classList.toggle("bg-dark");
+  document.querySelector(".entradas0").classList.toggle("dark-mode-color-entradas");
+  document.querySelector(".entradas1").classList.toggle("dark-mode-color-entradas");
+  document.querySelector(".entradas2").classList.toggle("dark-mode-color-entradas");
+
 }
